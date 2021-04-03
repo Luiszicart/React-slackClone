9aacef1ce2effd953929f1e564901b8a9b64c1d0
Luis A. Zapata Icart <expansion88@pm.me>
2021-04-03 10:08:32 -0300

    Firebase config

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDRjiCKjfW51UvLnaOj8snkYlkhe47Q2aE",
+  authDomain: "slack-clone-16d54.firebaseapp.com",
+  projectId: "slack-clone-16d54",
+  storageBucket: "slack-clone-16d54.appspot.com",
+  messagingSenderId: "1093748049563",
+  appId: "1:1093748049563:web:1e5e1bd560a02df86d683e"
+}
