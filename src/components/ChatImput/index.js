248e66fe8ae80670acab09dbdb1f1b81234b4e28
Luis A. Zapata Icart <expansion88@pm.me>
2021-04-08 23:34:19 -0300

    Chat Box (Must be debugged)

---
+++ src/components/ChatImput/index.js
@@ -0,0 +1,66 @@
+import React, { useState } from 'react'
+import { Button } from '@material-ui/core'
+import styled from 'styled-components'
+import { db } from '../../firebase'
+import firebase from 'firebase'
+
+
+function ChatImput({ channelName, channelId }) {
+  const [input, setInput] = useState('')
+
+  const sendMessage = (e) => {
+    e.preventDefault() // Prevents refresh
+
+    if (!channelId) {
+      return false
+    }
+
+    db.collection('rooms').doc(channelId).collection('messages').add({
+      message: input,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      user: 'Luis',
+      userImage: 'https://static.wikia.nocookie.net/naruto/images/f/fd/Sasuke_Uchiha_Parte_III_Anime.png/revision/latest?cb=20161210224058&path-prefix=es',
+    })
+
+    setInput('')
+  }
+
+  return (
+    <ChatImputContainer>
+      <form>
+        <input value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder={`Message #${channelName}`} />
+        <Button hidden type='submit' onClick={sendMessage}>
+          SEND
+        </Button>   
+      </form>
+    </ChatImputContainer>
+  )
+}
+
+export default ChatImput
+
+const ChatImputContainer = styled.div `
+  border-radius: 20px;
+
+  > form {
+    position: relative;
+    display: flex;
+    justify-content: center;
+  }
+
+  > form > input {
+    position: fixed;
+    bottom: 30px;
+    width: 60%;
+    border: 1px solid gray;
+    border-radius: 3px;
+    padding: 20px;
+    outline: none;
+  }
+
+  > form > Button {
+    display: none !important;
+  }
+`
